7bd207643cbbfeb93e477e55ca28a4ff50be2f9c
tvbach-20 <truong.viet.bach@vareal.vn>
2021-10-22 17:21:03 +0700

    work 1

--- src/components/todolist/data.js
+++ src/components/todolist/data.js
@@ -1,8 +1,8 @@
 const listData = [
-  {work: "Learn React js "},
-  {work: "Learn Deploy Production "},
-  {work: "Excerside "},
-  {work: "Reading Book"}
+  {work: "Learn React js ", status: 0},
+  {work: "Learn Deploy Production ", status: 0},
+  {work: "Excerside ", status: 0},
+  {work: "Reading Book", status: 0}
 ]
 
 export default listData;
